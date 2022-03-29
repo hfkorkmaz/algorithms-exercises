2c4d0c6baa656245f89499ae2ac6fbba642cdf69
fkorkmaz <furkan.korkmaz@hepsiburada.com>
2022-03-29 15:54:35 +0300

    Bubble Sort

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -11,12 +11,42 @@
 */
 
 function bubbleSort(nums) {
-  // code goes here
+  const arr = [...nums];
+
+  const sort = (arr, range) => {
+    if(range === 0){
+      return;
+    }
+
+    let swapHappened = false;
+
+    for (let i = 0; i < range; i++) {
+      if (i + 1 === range) {
+        break;
+      }
+
+      const element = arr[i];
+      const nextElement = arr[i + 1];
+
+      if (element > nextElement) {
+        arr[i + 1] = element;
+        arr[i] = nextElement;
+        swapHappened = true;
+      }
+    }
+    if (swapHappened) {
+      sort(arr, range - 1);
+    }
+  };
+
+  sort(arr, arr.length);
+
+  return arr;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
