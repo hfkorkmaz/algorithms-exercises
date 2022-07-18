50f9644b0223be61d46555615e4fa4ec5d7a671d
fkorkmaz <furkan.korkmaz@hepsiburada.com>
2022-07-18 10:18:02 +0300

    Quick Sort - Refactor

--- specs/quick-sort/quick-sort.test.js
+++ specs/quick-sort/quick-sort.test.js
@@ -19,10 +19,17 @@ function quickSort(nums) {
 
   const pivot = nums[nums.length - 1];
 
-  const numsWithoutPivot = nums.filter((_, index) => index !== nums.length - 1);
+  const left = [];
+  const right = [];
 
-  const left = numsWithoutPivot.filter((num) => num <= pivot);
-  const right = numsWithoutPivot.filter((num) => num >= pivot);
+  for (let i = 0; i < nums.length - 1; i++) {
+    const num = nums[i];
+    if (num <= pivot) {
+      left.push(num);
+      continue;
+    }
+    right.push(num);
+  }
 
   return [...quickSort(left), pivot, ...quickSort(right)];
 }
